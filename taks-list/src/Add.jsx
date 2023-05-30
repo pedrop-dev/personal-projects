542c6515c07d431fe5469f5b330655a10ce6d0a2
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-05-30 20:24:38 -0300

    First style for Add Task page

--- taks-list/src/Add.jsx
+++ taks-list/src/Add.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from  'react-router-dom'
+import AddStyle from './style/components/_Add.scss'
 
 export default function Add() {
     return (
-        <h1>ADICIONAR</h1>
+        <>
+            <main>
+                <section>
+                    <form>
+                        <label htmlFor="task">
+                            Task
+                        </label>
+                        <input type="text" id="task" />
+                    </form>
+                    <Link to='/'>
+                        <button>Add</button>
+                    </Link>
+                </section>
+            </main>
+        </>
     )
 }
