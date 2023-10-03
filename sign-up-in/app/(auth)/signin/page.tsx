ba629c807a8c67bf665410a7212425341deca396
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-10-03 16:15:53 -0300

    sign in page style

--- sign-up-in/app/(auth)/signin/page.tsx
+++ sign-up-in/app/(auth)/signin/page.tsx
@@ -1,7 +1,47 @@
+'use client'
+
+import { BiSolidLockAlt } from "react-icons/bi"
+import { IoMail } from "react-icons/io5"
+
 export default function Signin() {
     return (
         <>
-            <h1>Sign In PAGE</h1>
+            <main className="h-screen w-full bg-bg pt-24">
+                <h1 className="text-5xl text-center font-semibold font-sans text-white">
+                    Sign In to your account
+                </h1>
+                
+                <form className="w-full flex flex-col items-center gap-8 pt-24">
+                    <div className="w-full flex flex-row items-center justify-center">
+
+                        <IoMail className="h-14 pl-2 pr-1 text-4xl bg-bg text-d-white border-solid border-t-2 border-l-2 border-b-2 border-d-white rounded-sm " />
+
+                        <input 
+                            type="text" 
+                            placeholder="Email"
+                            className="h-14 w-8/12 pl-4 text-d-white  outline-none bg-bg border-solid border-t-2 border-r-2 border-b-2 border-d-white rounded-sm sm:w-7/12 md:w-6/12 lg:w-5/12 xl:w-4/12"
+                        />
+                    </div>
+
+                    <div className="w-full flex flex-row items-center justify-center">
+
+                        <BiSolidLockAlt className="h-14 pl-2 pr-1 text-4xl bg-bg text-d-white border-solid border-t-2 border-l-2 border-b-2 border-d-white rounded-sm " />
+
+                        <input 
+                            type="password" 
+                            placeholder="Password"
+                            className="h-14 w-8/12 pl-4 text-d-white outline-none bg-bg border-solid border-t-2 border-r-2 border-b-2 border-d-white rounded-sm sm:w-7/12 md:w-6/12 lg:w-5/12 xl:w-4/12"
+                        />
+                    </div>
+                    
+                    <button 
+                        type="submit"
+                        className="h-14 w-9/12 text-d-white border-solid border-2 border-d-white rounded-sm sm:w-7/12 md:w-6/12 lg:w-5/12 xl:w-4/12"
+                    >
+                        Sign In
+                    </button>
+                </form>
+            </main>
         </>
     )
 }
--- sign-up-in/app/(auth)/signup/page.tsx
+++ sign-up-in/app/(auth)/signup/page.tsx
@@ -3,17 +3,13 @@
 import { BiSolidLockAlt } from "react-icons/bi"
 import { IoMail } from "react-icons/io5"
 
-
-
 export default function Signup() {
     return (
         <>
-            
             <main className="h-screen w-full bg-bg pt-24">
                 <h1 className="text-5xl text-center font-semibold font-sans text-white">
                     Create your account
                 </h1>
-
                 
                 <form className="w-full flex flex-col items-center gap-8 pt-24">
                     <div className="w-full flex flex-row items-center justify-center">
@@ -23,7 +19,7 @@ export default function Signup() {
                         <input 
                             type="text" 
                             placeholder="Email"
-                            className="h-14 w-8/12 pl-4 text-d-white  outline-none bg-bg border-solid border-t-2 border-r-2 border-b-2 border-d-white rounded-sm"
+                            className="h-14 w-8/12 pl-4 text-d-white  outline-none bg-bg border-solid border-t-2 border-r-2 border-b-2 border-d-white rounded-sm sm:w-7/12 md:w-6/12 lg:w-5/12 xl:w-4/12"
                         />
                     </div>
 
@@ -34,13 +30,13 @@ export default function Signup() {
                         <input 
                             type="password" 
                             placeholder="Password"
-                            className="h-14 w-8/12 pl-4 text-d-white outline-none bg-bg border-solid border-t-2 border-r-2 border-b-2 border-d-white rounded-sm"
+                            className="h-14 w-8/12 pl-4 text-d-white outline-none bg-bg border-solid border-t-2 border-r-2 border-b-2 border-d-white rounded-sm sm:w-7/12 md:w-6/12 lg:w-5/12 xl:w-4/12"
                         />
                     </div>
                     
                     <button 
                         type="submit"
-                        className="h-14 w-9/12 text-d-white border-solid border-2 border-d-white rounded-sm"
+                        className="h-14 w-9/12 text-d-white border-solid border-2 border-d-white rounded-sm sm:w-7/12 md:w-6/12 lg:w-5/12 xl:w-4/12"
                     >
                         Sign Up
                     </button>
