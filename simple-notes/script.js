0f4ac7ef77a3125b71d2a181ca91010a9c6dc3c2
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-10-13 20:14:23 -0300

    feat: prisma

--- simple-notes/script.js
+++ simple-notes/script.js
@@ -28,7 +28,7 @@ addb.addEventListener('click', () => {
 
             const p = document.createElement("p")
             p.classList.add('pdescript')
-            div.appendChild(p)
+            div.appendChild(p)z
 
             h2.innerHTML += `${ititle.value}`
             p.innerHTML += `${idescript.value}`
